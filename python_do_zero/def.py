288d2d7cac109085c8d66f0bc03547d112552de2
mauricio girardi <mauriciogirardi@denox.com.br>
2021-07-11 22:17:04 -0300

    learning about function

---
+++ python_do_zero/def.py
@@ -0,0 +1,10 @@
+
+def msg_welcome(name):
+    print(f'Seja bem vinda a pastelaria {name} ;)')
+
+
+def add_order(customer, flavor):
+    new_order = f'O {customer} pediu pastel de {flavor}'
+    print(new_order)
+
+
