ad0f105af2d3d3599453e0e98949845d3e33503f
mauricio girardi <mauriciogirardi@denox.com.br>
2021-07-11 22:22:33 -0300

    learning about global variable

--- python_do_zero/def.py
+++ python_do_zero/def.py
@@ -1,3 +1,5 @@
+EMPRESA = 'MG'
+
 
 def msg_welcome(name):
     print(f'Seja bem vinda a pastelaria {name} ;)')
@@ -7,4 +9,11 @@ def add_order(customer, flavor):
     new_order = f'O {customer} pediu pastel de {flavor}'
     print(new_order)
 
+
+def imprima_nome():
+    name = 'Mauricio'
+    print(f'O {name} trabalha na {EMPRESA}')
+
+
+imprima_nome()
 
